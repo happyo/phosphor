3c97cb70591e1e195445baf33a3edea69e9028fb
happyo <happyojones@gmail.com>
2019-04-29 15:54:32 +0800

    add: login validation

--- user/static/user/javascript/login.js
+++ user/static/user/javascript/login.js
@@ -6,11 +6,15 @@ var user = new Vue({
     },
     methods: {
         login: function (event) {
-            apiSignIn({'username' : this.username, 'password' : this.password}).then(res => {
-                var data = res['data'];
-                var username = data['username'];
-                location.href = '/' + username + '/todos';
-            });
+            if (this.username.length == 0 || this.password.length == 0) {
+                vant.Toast('用户名密码不能为空');
+            } else {
+                apiSignIn({'username' : this.username, 'password' : this.password}).then(res => {
+                    var data = res['data'];
+                    var username = data['username'];
+                    location.href = '/' + username + '/todos';
+                });
+            }
         },
         register: function (event) {
             location.href = '/join';
